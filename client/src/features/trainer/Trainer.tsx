1702996556ef7a3e577842e106a3157e8e557ce3
alt2qplz <alexander.savelichev@gmail.com>
2021-01-23 01:06:21 +0300

    super-mvp, but it works!

--- client/src/features/trainer/Trainer.tsx
+++ client/src/features/trainer/Trainer.tsx
@@ -1,15 +1,38 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {newRandomCurrentItem, selectCurrentItem, selectItemsToChoose} from './trainerSlice'
+import {
+  newRandomCurrentItem,
+  selectCurrentItem,
+  selectItemsToChoose,
+  setChosenItem,
+  selectChosenItems,
+  removeChosenItem
+} from './trainerSlice'
+import {BaseItemType as BIT} from "../../types/itemTypes";
+import _ from "underscore";
 
 export const Trainer = () => {
   const currentItem = useSelector(selectCurrentItem)
   const itemsToChoose = useSelector(selectItemsToChoose)
+  const chosenItems = useSelector(selectChosenItems)
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(newRandomCurrentItem())
   }, [])
+
+  useEffect(() => {
+    if (itemsCheck(currentItem, chosenItems)) {
+      dispatch(newRandomCurrentItem())
+    }
+  }, [chosenItems])
+
+  const itemsCheck = (currentItem: BIT | null, chosenItems: BIT[]) => {
+    if (!currentItem || !currentItem.ingredients) {
+      return false
+    }
+    return _.difference(currentItem.ingredients, chosenItems.map(el => el.id)).length === 0;
+  }
 
   return (
     <div>
@@ -20,11 +43,13 @@ export const Trainer = () => {
       <p>
         Выбранные предметы:
       </p>
+      {chosenItems.map(el => <p key={el.id} onClick={() => dispatch(removeChosenItem(el.id))}>{el.name}</p>)}
       <hr/>
       <p>
         Выберите два предмета на выбор:
       </p>
-      {itemsToChoose.map(el => <p key={el.id}>{el.name}</p>)}
+      {itemsToChoose.map(el => <p key={el.id} onClick={() => dispatch(setChosenItem(el.id))}>{el.name}</p>)}
+      <hr/>
       <button onClick={() => dispatch(newRandomCurrentItem())}>
         click
       </button>
--- client/src/features/trainer/trainerSlice.ts
+++ client/src/features/trainer/trainerSlice.ts
@@ -1,14 +1,23 @@
-import { createSlice} from '@reduxjs/toolkit'
+import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 import { RootState } from '../../app/store'
 import {BaseItemType as BIT} from "../../types/itemTypes"
 import {baseState} from "../../state/baseState"
 import {ItemRarityEnum} from "../../types/enums";
+import _ from 'underscore'
 
 interface TrainerState {
   allItems: BIT[],
   currentItem: BIT | null,
   chosenItems: BIT[],
   itemsToChoose: BIT[]
+}
+
+// Тип у которого поле ингридиетов точно не undefined
+interface CurrentItemType {
+  id: number
+  name: string
+  rarity: ItemRarityEnum
+  ingredients: [number, number]
 }
 
 const initialState: TrainerState = {
@@ -23,21 +32,71 @@ export const trainerSlice = createSlice({
   initialState,
   reducers: {
     newRandomCurrentItem: state => {
-      let poolForRandomItem = state.allItems.filter(el => typeof el.ingredients !== "undefined")
-      poolForRandomItem = poolForRandomItem.filter(el => el.id <= 8) //only daggers
-      const newCurrentItem = poolForRandomItem[Math.floor(Math.random()*poolForRandomItem.length)]
+      // Создаем пул предметов, у которых есть инры (которые создаются из других предметов)
+      let poolForCurrentItem: CurrentItemType[] = state.allItems.filter(el  => el.ingredients !== undefined) as CurrentItemType[]
+      poolForCurrentItem = poolForCurrentItem.filter(el => el.id <= 8) //only daggers
+      // Случайно выбираем один из предметов, его рецепт и будем угадывать
+      const newCurrentItem = poolForCurrentItem[Math.floor(Math.random()*poolForCurrentItem.length)]
 
-      let poolForItemsToChoose = state.allItems.filter(el => el.rarity <= newCurrentItem.rarity)
+      // Созадем пул предметов из которых мог бы быть собрать текущий предмет, выкидывая все предметы, рариетет которых
+      // выше чем у текущего предмета
+      const poolForItemsToChoose: BIT[] = state.allItems.filter(el => el.rarity <= newCurrentItem.rarity)
 
-      const newItemsToChose = []
-      // @ts-ignore
-      newItemsToChose.push(poolForItemsToChoose.find(el => el.id === newCurrentItem.ingredients[0]))
-      // @ts-ignore
-      newItemsToChose.push(poolForItemsToChoose.find(el => el.id === newCurrentItem.ingredients[1]))
+      // Находим в этом пуле ПЕРВЫЙ необходимый для сборки предмет, записываем и вырезаем его и пула
+      const firstIngredientIdx = poolForItemsToChoose.findIndex(el => el.id === newCurrentItem.ingredients[0])
+      const firstIngredient: BIT = poolForItemsToChoose[firstIngredientIdx]
+      poolForItemsToChoose.splice(firstIngredientIdx, 1)
 
+      // Находим в этом пуле ВТОРОЙ необходимый для сборки предмет, записываем и вырезаем его и пула
+      const secondIngredientIdx = poolForItemsToChoose.findIndex(el => el.id === newCurrentItem.ingredients[1])
+      const secondIngredient: BIT = poolForItemsToChoose[secondIngredientIdx]
+      poolForItemsToChoose.splice(secondIngredientIdx, 1)
 
+      // Создаем массив из нескольких СЛУЧАЙНЫХ предметов
+      const randomItemArray: BIT[] = []
+      for (let i = 0; i < 3; i++) {
+        if (poolForItemsToChoose.length) {
+          const idx = Math.floor(Math.random()*poolForItemsToChoose.length)
+          const item: BIT = poolForItemsToChoose[idx]
+          poolForItemsToChoose.splice(idx, 1)
+          randomItemArray.push(item)
+        }
+      }
+
+      // Записываем все предметы (нужные и случайные) в один массив
+      const newItemsToChose: BIT[] = []
+      newItemsToChose.push(firstIngredient)
+      newItemsToChose.push(secondIngredient)
+      newItemsToChose.push(...randomItemArray)
+
+      // Устанавливаем значение ТЕКУЩЕГО угадываемого предмета
       state.currentItem = newCurrentItem
-      // state.itemsToChoose = newItemsToChose
+      // Устанавливаем ПЕРЕМЕШАННОЕ знаечение массива с предметами
+      state.itemsToChoose = _.shuffle(newItemsToChose)
+      // Очищаем массив выбранных предметов
+      state.chosenItems = []
+    },
+    setChosenItem: (state, action: PayloadAction<number>) => {
+      if (state.chosenItems.length < 2) {
+        const itemIdx = state.itemsToChoose.findIndex(el => el.id === action.payload)
+        const item: BIT = state.itemsToChoose[itemIdx]
+
+        const newItemsToChoose = state.itemsToChoose
+        newItemsToChoose.splice(itemIdx, 1)
+
+        state.itemsToChoose = newItemsToChoose
+        state.chosenItems = [...state.chosenItems, item]
+      }
+    },
+    removeChosenItem: (state, action: PayloadAction<number>) => {
+      const itemIdx = state.chosenItems.findIndex(el => el.id === action.payload)
+      const item: BIT = state.chosenItems[itemIdx]
+
+      const newChosenItems = state.chosenItems
+      newChosenItems.splice(itemIdx, 1)
+
+      state.chosenItems = newChosenItems
+      state.itemsToChoose = [...state.itemsToChoose, item]
     }
 
 
@@ -55,9 +114,10 @@ export const trainerSlice = createSlice({
   },
 });
 
-export const { newRandomCurrentItem } = trainerSlice.actions
+export const { newRandomCurrentItem, setChosenItem, removeChosenItem } = trainerSlice.actions
 
 export const selectCurrentItem = (state: RootState) => state.trainer.currentItem
 export const selectItemsToChoose = (state: RootState) => state.trainer.itemsToChoose
+export const selectChosenItems = (state: RootState) => state.trainer.chosenItems
 
 export default trainerSlice.reducer
